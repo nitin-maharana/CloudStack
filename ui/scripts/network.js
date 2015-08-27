5cfd4f84cb8dbfaa236a0510b7d4a2648b1ca933
Nitin Kumar Maharana <nitinkumar.maharana@citrix.com>
2015-08-27 12:08:38 +0530

    CLOUDSTACK-8672 Added code for disabling the SSL Certificate Configuration button (After being assigned to LB) in UI.

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -3624,12 +3624,10 @@
                                                     }
                                                 }
                                             },
-
                                             'add-vm': {
                                                 label: 'label.add.vms',
                                                 addButton: true
                                             },
-
                                             'state' : {
                                                 edit: 'ignore',
                                                 label: 'label.state'
@@ -4038,6 +4036,21 @@
                                                                 cloudStack.dialog.notice({
                                                                     message: parseXMLHttpResponse(json)
                                                                 });
+                                                            }
+                                                        });
+
+                                                        // Get SSL Certificate data
+                                                        $.ajax({
+                                                            url: createURL('listSslCerts'),
+                                                            data: {
+                                                                listAll: true,
+                                                                lbruleid: lbRule.id
+                                                            },
+                                                            async: false,
+                                                            success: function(json) {
+                                                                if (json.listsslcertsresponse != null) {
+                                                                    lbRule._hideFields.push('sslcertificate');
+                                                                }
                                                             }
                                                         });
 
