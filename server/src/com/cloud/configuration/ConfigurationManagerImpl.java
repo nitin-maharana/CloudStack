9a19548f21bfa9046cfa3ea12692819c4e799049
Rajesh Battala <rajesh.battala@citrix.com>
2015-09-27 08:34:17 +0530

    CLOUDSTACK-8672: Fixed Network offering to save servicepackage

--- server/src/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -4437,6 +4437,8 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
             offeringFinal.setServiceOfferingId(serviceOfferingId);
         }
 
+        //Set Service package id
+        offeringFinal.setServicePackage(servicePackageUuid);
         // validate the details
         if (details != null) {
             validateNtwkOffDetails(details, serviceProviderMap);
