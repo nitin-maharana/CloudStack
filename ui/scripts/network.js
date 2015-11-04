2ecde70a48b5e567ad64829b8d181165ec901153
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2015-11-04 12:25:21 +0530

    CLOUDSTACK-XXXX: Load Balancer configuration page does not have "Source" methond in the drop down list
    Reverting my previous commit.
    The SOURCE LB algorithm is not supported in case of NetScaler.

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -3490,6 +3490,10 @@
                                                             name: 'source',
                                                             description: _l('label.lb.algorithm.source')
                                                         }];
+                                                    if (typeof args.context != 'undefined') {
+                                                        var lbAlgs = getLBAlgorithms(args.context.networks[0]);
+                                                        data = (lbAlgs.length == 0) ? data : lbAlgs;
+                                                    }
                                                     args.response.success({
                                                         data: data
                                                     });
